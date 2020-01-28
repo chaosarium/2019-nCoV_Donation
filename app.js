5ebca05bdaf68fc452b35225afc2895d11701c88
Leon <leonluleonlu@gmail.com>
2020-01-28 21:40:40 +0800

    update index page

--- app.js
+++ app.js
@@ -27,15 +27,37 @@ app.use((req, res, next) => {
 //=================
 //====Variables====
 //=================
-const port = 2019;
-const url = "mongodb://localhost:27017";
-const dbName = "nCoV";
+const port = 2019
+const url = "mongodb://localhost:27017"
+const dbName = "nCoV"
 
 //=================
 //====Functions====
 //=================
+function getDateTime() {
+  var date = new Date();
+  
+  var hour = date.getHours();
+  hour = (hour < 10 ? "0" : "") + hour;
+  var min = date.getMinutes();
+  min = (min < 10 ? "0" : "") + min;
+  var sec = date.getSeconds();
+  sec = (sec < 10 ? "0" : "") + sec;
+  var milisec = date.getMilliseconds();
+  milisec = ((milisec < 100) ? ((milisec < 10) ? "00" : "0") : "") + milisec;
+  
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  month = (month < 10 ? "0" : "") + month;
+  var day = date.getDate();
+  day = (day < 10 ? "0" : "") + day;
+  
+  var timeArray = {year: year, month: month, day: day, hour: hour, min: min, sec: sec, milisec: milisec}
+  return timeArray;
+}
+
 //insert entry function
-function dbInsert(time, firstName, lastName, year, studentID) {
+function dbInsert(fullName, emailAddress, studentID) {
   //connect to mongo client
   MongoClient.connect(url, function(err, db) {
     if (err) console.log(err);
--- public/js/main.js
+++ public/js/main.js
@@ -232,3 +232,5 @@
 // $(document).ready(function() {
 // $('.mdb-select').materialSelect();
 // });
+
+
